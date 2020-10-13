4b4e6162805c609dc92ec0b887ab8cabb53574a8
Antonio Raimundo <antoniorrm@Mac-mini-Dj.local>
2020-10-13 11:44:53 -0300

    add images, page landing and routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
+import Routes from "./routes";
+
+import "./styles/global.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello Word</h1>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
