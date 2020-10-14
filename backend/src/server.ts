7bee095c64e43a836286f02a6b4ceedea3c72475
Antonio Raimundo <antoniorrm@Mac-mini-Dj.local>
2020-10-14 13:40:55 -0300

    add exception for errors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 
 import "./database/connection";
+import errorHandler from "./errors/handler";
 import routes from "./routes";
 
 const app = express();
@@ -9,5 +10,6 @@ const app = express();
 app.use(express.json())
 app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname,"..",  "uploads")));
+app.use(errorHandler)
 
 app.listen(3333);
