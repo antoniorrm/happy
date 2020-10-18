a08654d52e59bb40666b042a6db0b1de06b4ed4c
antoniorrm <antonioraimundo007@gmail.com>
2020-10-17 21:11:23 -0300

    header ok

--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from '@react-navigation/native';
 
-export default function Header(){
+interface headerProps{
+    title: string,
+    showCancel?: boolean
+}
+export default function Header({title, showCancel} : headerProps){
+    const navigation = useNavigation();
+
+    function handleGoBackToAppHomepage() {
+        navigation.navigate("OrphanagesMap")
+    }
+
     return (
         <View style={styles.container}>
-
+            <BorderlessButton onPress={navigation.goBack}>
+                <Feather name="arrow-left" size={24} color="#15b6d6" />
+            </BorderlessButton>
+            <Text style={styles.title}>{title}</Text>
+            {showCancel ? 
+            <BorderlessButton onPress={handleGoBackToAppHomepage}>
+                <Feather name="x" size={24} color="#ff669d" />
+            </BorderlessButton> : 
+            <View />
+            }
+            
         </View>
     )
 }
@@ -12,7 +35,7 @@ export default function Header(){
 const styles = StyleSheet.create({
     container: {
         padding: 24,
-        backgroundColor: "#f9f4fc",
+        backgroundColor: "#f9fafc",
         borderBottomWidth: 1,
         borderColor: "#dde3f0",
         paddingTop: 44,
@@ -21,4 +44,9 @@ const styles = StyleSheet.create({
         justifyContent: "space-between",
         alignItems: "center",
     },
+    title : {
+        fontFamily: "Nunito_600SemiBold",
+        color: "#8fa7b5",
+        fontSize: 16,
+    }
 })
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -9,15 +9,27 @@ import OrphanagesMap from "./pages/OrphanagesMap";
 import OrphanageDetails from "./pages/OrphanageDetails";
 import SelectMapPosition from "./pages/SelectMapPosition";
 import OrphanageData from "./pages/OrphanageData";
+import Header from "./components/Header";
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
+      <Navigator screenOptions={{ headerShown: false, cardStyle: {backgroundColor: "#f2f3f5"} }}>
         <Screen name="OrphanagesMap" component={OrphanagesMap} />
-        <Screen name="OrphanageDetails" component={OrphanageDetails} />
-        <Screen name="SelectMapPosition" component={SelectMapPosition} />
-        <Screen name="OrphanageData" component={OrphanageData} />
+        <Screen name="OrphanageDetails" component={OrphanageDetails} 
+          options={{
+          headerShown: true,
+          header: () => <Header title="Orfanatos" showCancel={false}/>
+        }}
+        />
+        <Screen name="SelectMapPosition" component={SelectMapPosition} options={{
+          headerShown: true,
+          header: () => <Header title="Selecione no mapa" showCancel={false}/>
+        }}/>
+        <Screen name="OrphanageData" component={OrphanageData} options={{
+          headerShown: true,
+          header: () => <Header title="Informe os dados" showCancel={true}/>
+        }}/>
       </Navigator>
     </NavigationContainer>
   );
